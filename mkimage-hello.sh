2d5716cddaa12d9e103a3169219fa8076865f710
Buodaxuo8IeS <MoGh4aaQuaij@yandex.com>
2013-05-25 01:01:33 +0000

    mkimage-hello.sh now uses Dockerfile.hello

--- mkimage-hello.sh
+++ mkimage-hello.sh
@@ -1,46 +1,9 @@
 #!/bin/bash
-# Generate a very minimal filesystem based on busybox-static,
-# and load it into the local docker under the name "busybox".
-
-BUSYBOX=$(which busybox)
-[ "$BUSYBOX" ] || {
-    echo "Sorry, I could not locate busybox."
-    echo "Try 'apt-get install busybox-static'?"
-    exit 1
-}
-
-HELLO=$(which hello)
-[ "$HELLO" ] || {
-    echo "Sorry, I could not locate hello."
-    exit 1
-}
 
 set -e
-ROOTFS=/tmp/rootfs-busybox-$$-$RANDOM
-mkdir $ROOTFS
-cd $ROOTFS
 
-mkdir bin etc dev dev/pts lib proc sys tmp
-touch etc/resolv.conf
-cp /etc/nsswitch.conf etc/nsswitch.conf
-echo root:x:0:0:root:/:/bin/sh > etc/passwd
-echo root:x:0: > etc/group
-ln -s lib lib64
-ln -s bin sbin
-cp $BUSYBOX bin
-cp $HELLO bin
-for X in $(busybox --list)
-do
-    ln -s busybox bin/$X
-done
-rm bin/init
-ln bin/busybox bin/init
-cp /lib/x86_64-linux-gnu/lib{pthread,c,dl,nsl,nss_*}.so.* lib
-cp /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 lib
-for X in console null ptmx random stdin stdout stderr tty urandom zero
-do
-    sudo cp -a /dev/$X dev
-done
+IMG_ID=$(docker build - < Dockerfile.hello | grep "image id:" | sed 's/.*image id: \(.*\)/\1/')
+echo IMG_ID: $IMG_ID
+docker rmi hello
+docker tag $IMG_ID hello
 
-tar -cf- . | docker import - hello
-docker run -i -u root hello /bin/echo Success.
